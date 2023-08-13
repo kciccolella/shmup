dec6a5c7723b2061c6a8f41bcd58c811036442c0
kciccolella <26233701+kciccolella@users.noreply.github.com>
2023-08-13 09:09:06 -0400

    added speed streaks, planets, and stars, and refactored code

--- shmup.js
+++ shmup.js
@@ -43,19 +43,9 @@ collision = false;
 }
 
 var player;
-/*
-var player = new Sprite({
-  name: 'player',
-  position: {
-    x: 100,
-    y: 100
-  },
-  velocity: {
-    x: 0,
-    y: 0
-  },
-});
-*/
+var streaks = 0;
+
+generateStreaks();
 generatePlayer();
 generateEnemy();
 generateAsteroid();
@@ -64,25 +54,37 @@ function draw() {
   // Background
   ctx.fillStyle = 'black';
   ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  stars();
+  planet(168, 20, true);
+  planet(30, 100, false);
+  if (streaks < 5) {
+    generateStreaks();
+  }
 }
 
 function update() {
   draw();
   // console.log('update');
 
-  // Player
-  // ctx.fillStyle = 'blue';
+  // Find player sprite
   player = sprites.find(function(sprite) {
     return sprite.name === 'player';
   });
 
-  // Sprites
+  // Sprite actions
   for (var i = 0; i < sprites.length;i++) {
     var sprite = sprites[i];
-    if (sprite.name === 'player') {
+    if (sprite.name === 'streak') {
+      ctx.fillStyle = 'white';
+      showSpeed(sprite);
+      sprite.position.y += sprite.velocity.y;
+    } else if (sprite.name === 'player') {
       ctx.fillStyle = 'blue';
       playerSkin(sprite);
       sprite.position.x += sprite.velocity.x;
+      playerMovement(sprite);
+      bulletFn();
     } else if (sprite.name === 'bullet') {
       ctx.fillStyle = 'yellow';
       bulletSkin(sprite);
@@ -96,30 +98,6 @@ function update() {
       enemySkin(sprite);
       sprite.position.y += sprite.velocity.y;
     }
-  }
-
-  // Player Movement
-  if (player) {
-    player.velocity.x = 0;
-    if (keys.left.pressed === true &&
-        player.lastKey === 'left' &&
-        player.position.x + -1 >= 0) {
-      player.velocity.x = -2;
-    } else if (keys.right.pressed === true &&
-              player.lastKey === 'right' &&
-              player.position.x + player.width + 2 <= canvas.width) {
-      player.velocity.x = 2;
-    }
-  }
-
-  // Player Bullet
-  if (keys.space.pressed === true && !player.overheated) {
-    generateBullet();
-    player.overheated = true;
-    setTimeout(function() {
-      player.overheated = false;
-    }, 500);
-    player.weaponL = !player.weaponL;
   }
 
   collisionDetection();
@@ -155,8 +133,8 @@ function generateBullet() {
   sprites.push(new Sprite({
     name: 'bullet',
     position: {
-      x: player.position.x + (player.weaponL ? +4 : +3),
-      y: player.position.y - 2
+      x: player.position.x + (player.weaponL ? 4 : 3),
+      y: player.position.y + 1
     },
     velocity: {
       x: 0,
@@ -190,7 +168,7 @@ function generateEnemy() {
     },
     velocity: {
       x: 0,
-      y: 1
+      y: rng(1)
     }
   }));
 }
@@ -199,24 +177,30 @@ function garbageCollection() {
   for (var i = 0; i < sprites.length; i++) {
     if (sprites[i].name === 'player') {
       if (sprites[i].collision) {
-        console.log('player collected');
+        // console.log('player collected');
         delete sprites[i];
       }
     } else if (sprites[i].name === 'bullet') {
       sprites[i].alive++;
       if (sprites[i].collision || sprites[i].position.y + 0 <= 0) {
-        console.log('bullet collected');
+        // console.log('bullet collected');
         delete sprites[i];
       }
     } else if (sprites[i].name === 'asteroid') {
       if (sprites[i].collision || sprites[i].position.y + 0 >= canvas.height) {
-        console.log('asteroid collected');
+        // console.log('asteroid collected');
         delete sprites[i];
       }
     } else if (sprites[i].name === 'enemy') {
       if (sprites[i].collision || sprites[i].position.y + 0 >= canvas.height) {
-        console.log('enemy collected');
+        // console.log('enemy collected');
         delete sprites[i];
+      }
+    } else if (sprites[i].name === 'streak') {
+      if (sprites[i].position.y + 0 >= canvas.height) {
+        // console.log('streak collected');
+        delete sprites[i];
+        streaks--;
       }
     }
   }
@@ -241,7 +225,7 @@ function playerSkin(sprite) {
 function bulletSkin(sprite) {
   // ctx.fillStyle = 'white';
   var length = sprite.alive;
-  if (length < 3) {
+  if (length < 2) {
     ctx.fillRect(sprite.position.x, sprite.position.y, 1, length);
   } else {
     ctx.fillRect(sprite.position.x, sprite.position.y, 1, 6);
@@ -332,14 +316,23 @@ Two loops and a map? Time O(n)
 
 function collisionDetection() {
   for (var i = 0; i < sprites.length; i++) {
+    if (sprites[i].name === 'streak') {
+      continue;
+    }
+
     for (var j = i + 1; j < sprites.length; j++) {
+      if (sprites[j].name === 'streak') {
+        continue;
+      }
+
       var spriteA = sprites[i];
       var spriteB = sprites[j];
 
       if (spriteA.position.x + spriteA.width >= spriteB.position.x &&
-        spriteA.position.x <= spriteB.position.x + spriteB.width &&
-        spriteA.position.y + spriteA.width >= spriteB.position.y &&
-        spriteA.position.y <= spriteB.position.y + spriteB.width) {
+          spriteA.position.x <= spriteB.position.x + spriteB.width &&
+          spriteA.position.y + spriteA.width >= spriteB.position.y &&
+          spriteA.position.y <= spriteB.position.y + spriteB.width) {
+        console.log(spriteA.name, 'and', spriteB.name, 'have collided');
         spriteA.collision = true;
         spriteB.collision = true;
       }
@@ -356,3 +349,127 @@ function collisionExplosion() {
   }
 }
 
+function playerMovement(sprite) {
+  player.velocity.x = 0;
+  if (keys.left.pressed &&
+      sprite.lastKey === 'left' &&
+      sprite.position.x + -1 >= 0) {
+    sprite.velocity.x = -2;
+  } else if (keys.right.pressed &&
+            sprite.lastKey === 'right' &&
+            sprite.position.x + sprite.width + 2 <= canvas.width) {
+    sprite.velocity.x = 2;
+  }
+}
+
+function bulletFn() {
+  // Player Bullet
+  if (keys.space.pressed && !player.overheated) {
+    generateBullet();
+    player.overheated = true;
+    setTimeout(function() {
+      if (player) {
+        player.overheated = false;
+      }
+    }, 500);
+    player.weaponL = !player.weaponL;
+  }
+}
+
+function planet(x, y, ring) {
+  ctx.fillStyle = 'brown';
+  ctx.fillRect(x + 9, y + 0, 6, 16);
+  ctx.fillRect(x + 7, y + 1, 10, 14);
+  ctx.fillRect(x + 6, y + 2, 12, 12);
+  ctx.fillRect(x + 5, y + 3, 14, 10);
+  ctx.fillRect(x + 4, y + 5, 16, 6);
+
+  if (ring) {
+    ctx.fillStyle = 'grey';
+    ctx.fillRect(x + 21, y + 2, 3, 4);
+    ctx.fillRect(x + 20, y + 3, 4, 3);
+    ctx.fillRect(x + 19, y + 3, 1, 2);
+    ctx.fillRect(x + 19, y + 6, 4, 1);
+    ctx.fillRect(x + 16, y + 7, 5, 1);
+    ctx.fillRect(x + 13, y + 8, 6, 1);
+    ctx.fillRect(x + 10, y + 9, 6, 1);
+    ctx.fillRect(x + 7, y + 10, 6, 1);
+    ctx.fillRect(x + 7, y + 11, 3, 1);
+    ctx.fillRect(x + 0, y + 11, 7, 2);
+    ctx.fillRect(x + 0, y + 11, 4, 3);
+    ctx.fillRect(x + 1, y + 10, 4, 1);
+    ctx.fillRect(x + 2, y + 9, 2, 1);
+  }
+}
+
+function stars() {
+  ctx.fillStyle = 'white';
+  ctx.fillRect(175, 5, 1, 1);
+  ctx.fillRect(67, 8, 1, 1);
+  ctx.fillRect(31, 20, 1, 1);
+  ctx.fillRect(113, 20, 1, 1);
+  ctx.fillRect(59, 20, 1, 3);
+  ctx.fillRect(58, 21, 3, 1);
+  ctx.fillRect(181, 25, 1, 1);
+  ctx.fillRect(16, 31, 1, 1);
+  ctx.fillRect(73, 45, 1, 1);
+  ctx.fillRect(104, 46, 1, 1);
+  ctx.fillRect(156, 47, 1, 1);
+  ctx.fillRect(39, 48, 1, 1);
+  ctx.fillRect(130, 53, 1, 1);
+  ctx.fillRect(172, 60, 1, 1);
+  ctx.fillRect(100, 61, 1, 1);
+  ctx.fillRect(111, 70, 1, 1);
+  ctx.fillRect(154, 70, 1, 1);
+  ctx.fillRect(91, 71, 1, 1);
+  ctx.fillRect(99, 72, 1, 1);
+  ctx.fillRect(152, 73, 1, 1);
+  ctx.fillRect(157, 73, 1, 1);
+  ctx.fillRect(44, 75, 1, 5);
+  ctx.fillRect(155, 75, 1, 1);
+  ctx.fillRect(43, 77, 3, 1);
+  ctx.fillRect(22, 80, 1, 1);
+  ctx.fillRect(75, 85, 1, 1);
+  ctx.fillRect(110, 85, 1, 3);
+  ctx.fillRect(109, 86, 3, 1);
+  ctx.fillRect(178, 90, 1, 1);
+  ctx.fillRect(136, 93, 1, 1);
+  ctx.fillRect(59, 99, 1, 1);
+  ctx.fillRect(103, 106, 1, 1);
+  ctx.fillRect(18, 107, 1, 1);
+  ctx.fillRect(179, 112, 1, 1);
+  ctx.fillRect(40, 120, 1, 1);
+  ctx.fillRect(18, 121, 1, 1);
+  ctx.fillRect(128, 128, 1, 1);
+  ctx.fillRect(60, 130, 1, 1);
+  ctx.fillRect(150, 131, 1, 3);
+  ctx.fillRect(149, 132, 3, 1);
+  ctx.fillRect(107, 133, 1, 1);
+  ctx.fillRect(18, 141, 1, 1);
+  ctx.fillRect(175, 142, 1, 1);
+  ctx.fillRect(87, 144, 1, 1);
+  ctx.fillRect(12, 147, 1, 1);
+  ctx.fillRect(147, 148, 1, 1);
+  ctx.fillRect(17, 154, 1, 1);
+}
+
+function showSpeed(sprite) {
+  ctx.fillRect(sprite.position.x, sprite.position.y + sprite.velocity.y, 1, sprite.velocity.y);
+}
+
+function generateStreaks() {
+  streaks++;
+  sprites.push(new Sprite({
+    name: 'streak',
+    position: {
+      x: rng(184),
+      y: rng(160) - 8
+    },
+    velocity: {
+      x: 0,
+      y: 1
+    },
+    width: 1,
+    height: 1
+  }));
+}
